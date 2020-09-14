708abd1ccf849f7a76ecc5a8ec7c160001a1d404
995008651@qq.com <995008651@qq.com>
2020-09-14 16:34:18 +0800

    test

---
+++ src/main/java/model/test.java
@@ -0,0 +1,10 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class test {
+    private String name;
+    private String id;
+    private String password;
+}
---
+++ src/main/java/model/usermodel.java
@@ -0,0 +1,6 @@
+package model;
+
+public class usermodel {
+    private String USERNAME;
+    private String clothes;
+}
